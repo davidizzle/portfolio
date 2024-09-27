4f94d8d9e94bf88c9212f2dafd6a1c0b779aa76c
davidizzle <davide.ferretti.j@gmail.com>
2024-09-27 20:55:45 +0200

    3D objects

---
+++ py_make3d.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import binvox_rw
+from scipy.spatial.transform import Rotation as R
+
+# Full rotation matrix input parameters
+theta_x = 0
+theta_y = 0
+theta_z = 0
+
+# ot_x = np.array([[1, 0, 0]
+# Don't do full rotation matrix just import it from SciPy
+
+rotation = R.from_euler('xyz', [theta_x, theta_y, theta_z])
+rotation_matrix = rotation.as_matrix()
+
+with open('todo.binvox', 'rb') as f:
+    md = binvox_rw.read_as_coord_array(f)
+
+# Rotate
+md = np.dot(rotation_matrix, md)
+
+x = md.data[0,:]
+y = md.data[1,:]
+z = md.data[2,:]
+
+grid_size = (50, 50, 50)
+
+# Initialize an empty voxel grid with False values
+voxel_grid = np.zeros(grid_size, dtype=bool)
+
+# Normalize coordinates to fit the grid (you may need to scale your data)
+x_norm = np.interp(x, (x.min(), x.max()), (0, grid_size[0]-1)).astype(int)
+y_norm = np.interp(y, (y.min(), y.max()), (0, grid_size[1]-1)).astype(int)
+z_norm = np.interp(z, (z.min(), z.max()), (0, grid_size[2]-1)).astype(int)
+
+# Mark the corresponding voxel grid points as True
+voxel_grid[x_norm, y_norm, z_norm] = True
+
+# Set up the 3D plot
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111, projection='3d')
+
+# Create the voxel plot
+ax.voxels(voxel_grid, facecolors='blue', edgecolor='k')
+
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+
+plt.show()
+
