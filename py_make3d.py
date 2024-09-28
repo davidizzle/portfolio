117d00c9e74ca5ca8b2a6e91a45fa8d010f70803
davidizzle <davide.ferretti.j@gmail.com>
2024-09-28 14:03:28 +0200

    JSON

--- py_make3d.py
+++ py_make3d.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import binvox_rw
 from scipy.spatial.transform import Rotation as R
+import json
 
 # Full rotation matrix input parameters
 theta_x = 0
@@ -26,7 +27,7 @@ y = md.data[1,:]
 z = md.data[2,:]
 
 # grid_size = (50, 50, 50)
-# grid_size = (30, 30, 30)
+grid_size = (30, 30, 30)
 
 # Initialize an empty voxel grid with False values
 voxel_grid = np.zeros(grid_size, dtype=bool)
@@ -35,7 +36,7 @@ voxel_grid = np.zeros(grid_size, dtype=bool)
 x_norm = np.interp(x, (x.min(), x.max()), (0, grid_size[0]-1)).astype(int)
 y_norm = np.interp(y, (y.min(), y.max()), (0, grid_size[1]-1)).astype(int)
 z_norm = np.interp(z, (z.min(), z.max()), (0, grid_size[2]-1)).astype(int)
-
+print(x_norm.shape)
 # Mark the corresponding voxel grid points as True
 voxel_grid[x_norm, y_norm, z_norm] = True
 
@@ -52,3 +53,14 @@ ax.set_zlabel('Z axis')
 
 plt.show()
 
+
+# array = voxel_grid.astype(int)
+array = np.where(voxel_grid == True)
+result = []
+result.append([int(k) - 15 for k in array[0].tolist()])
+result.append([int(k) - 15 for k in array[1].tolist()])
+result.append([int(k) - 24 for k in array[2].tolist()])
+
+# Save the array as a JSON file
+with open('array_data.json', 'w') as json_file:
+    json.dump(result, json_file)
